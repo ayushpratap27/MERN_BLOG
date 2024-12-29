43ee32f895d3b77a87e78aec7c31b0b5d922c161
Ayush Pratap <ayushpratap2127@gmail.com>
2024-12-29 16:31:27 +0530

    Added edit functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -84,6 +84,14 @@ const handleLike = async (commentId) => {
   }
 };
 
+const handleEdit = async (comment, editedContent) => {
+  setComments(
+    comments.map((c) =>
+      c._id === comment._id ? { ...c, content: editedContent } : c
+    )
+  );
+};
+
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
@@ -131,7 +139,7 @@ const handleLike = async (commentId) => {
             </div>
           </div>
           {comments.map((comment) => (
-            <Comment key={comment._id} comment={comment} onLike={handleLike} />
+            <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
           ))}
         </>
       )}
